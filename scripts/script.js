2ece00de808e79aed837a842ea7dda4dcc96304d
Abhishek250202 <89466445+Abhishek250202@users.noreply.github.com>
2022-11-13 23:52:46 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,105 @@
+console.log("ready...")
+
+// document.querySelector('.hamburger').addEventListener("click",()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+//     document.querySelector('.main').classList.toggle('mainmove');
+//     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+//             document.getElementById('ham').src="/img/ham.png"
+//         }
+//         else{
+//         document.getElementById('ham').src="/img/back.png"
+//     }
+// })
+
+// document.querySelector('.cross').addEventListener("click",()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+//     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+//         document.querySelector('.ham').style.display = 'inline'
+//         document.querySelector('.cross').style.display = 'none'
+//     }
+//     else{
+//         document.querySelector('.cross').style.display = 'inline'
+//         document.querySelector('.ham').style.display = 'none'
+//     }
+// })
+
+//Display certificates on hover
+// var flag=0
+
+
+
+// document.querySelector('.certificatelist').style.display = 'none'
+
+// })
+
+
+
+
+
+
+
+
+// document.querySelector('.name_1').addEventListener("mouseover",()=>{
+//     // console.log('IBM working')
+//     document.querySelector('.ibm').style.display = ''
+// })
+
+// if(flag===0)
+// {
+
+// }
+// else{
+//     document.querySelector('.ibm').style.display = ''
+// }
+
+
+// document.querySelector('.name_2').addEventListener("mouseover",()=>{
+//     // console.log('GOOGLE working')
+//     document.querySelector('.google').style.display = ''
+// })
+// document.querySelector('.name_2').addEventListener("mouseout",()=>{
+//     document.querySelector('.google').style.display = 'none'
+// })
+
+
+// document.querySelector('.name_3').addEventListener("mouseover",()=>{
+//     // console.log('Yale working')
+//     document.querySelector('.yale').style.display = ''
+// })
+// document.querySelector('.name_3').addEventListener("mouseout",()=>{
+//     document.querySelector('.yale').style.display = 'none'
+// })
+
+
+// document.querySelector('.name_4').addEventListener("mouseover",()=>{
+//     // console.log('NASA working')
+//     document.querySelector('.nasa').style.display = ''
+// })
+// document.querySelector('.name_4').addEventListener("mouseout",()=>{
+//     document.querySelector('.nasa').style.display = 'none'
+// })
+
+
+// document.querySelector('.name_5').addEventListener("mouseover",()=>{
+//     // console.log('other working')
+//     document.querySelector('.other').style.display = ''
+// })
+// document.querySelector('.name_5').addEventListener("mouseout",()=>{
+//     document.querySelector('.other').style.display = 'none'
+// })
+
+// //Display certificates on click
+// document.querySelector('.name_1').addEventListener("click",()=>{
+//     // console.log('IBM click working')
+//     document.querySelector('.ibm').style.display = ''
+// })
+// function runt(){
+//     // console.log('hello')
+//     var div = document.createElement("dive");
+//     // div.addEventListener("click",runt)
+//     this.append(div)
+
+
+
+// }
+// document.querySelector(".hand").addEventListener("click", runt)
